17bf0526acd821185600b2e56d6d58770c144fd2
JurRter <brendo.henrique@mail.uft.edu.br>
2026-03-04 23:52:32 -0300

    teste bleeeh

---
+++ main.php
@@ -0,0 +1,20 @@
+<?php
+$titulo = "cuzin!";
+$mensagem = "como q eu ponho a porra do html ai tyle.css.";
+?>
+<!doctype html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Main + PHP + Style</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<main class="container">
+		<h1><?php echo $titulo; ?></h1>
+		<p><?php echo $mensagem; ?></p>
+	</main>
+</body>
+</html>
+
